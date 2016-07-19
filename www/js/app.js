8aad18ccfc3684384da09091fc96f6d70058e9d1
Trung Trinh <trinhtoantrung@gmail.com>
2016-07-19 23:04:29 +0700

    Commit my code

---
+++ www/js/app.js
@@ -0,0 +1,19 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      var push = new Ionic.Push({
+        "debug": true
+      });
+
+      push.register(function (token) {
+        console.log("Device token:", token.token);
+        push.saveToken(token);  // persist the token in the Ionic Platform
+      });
+    });
+  })
